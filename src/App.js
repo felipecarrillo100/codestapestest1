d983f8bd60d061f73ec4ed0c99753f3b83b97808
felipecarrillo100 <felipecarrillo100@gmail.com>
2022-08-27 21:11:28 +0000

    First

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import "./App.css";
+const App = () => {
+  return (
+    <div>
+      <h1 className='heading__one'>React application from codespaces 4!!!</h1>
+    </div>
+  );
+};
+
+export default App;
